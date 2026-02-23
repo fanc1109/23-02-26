8f5fbf082ce091fe99b588f3c82395ee97e12578
Francy L. Schenkel <francylauana11@gmail.com>
2026-02-23 14:43:40 -0300

    Add button click behavior and stylesheet link
    Link a new styles.css in index.html and add IDs to the paragraph and button. Replace the alert in scritps.js with a click event listener that updates the paragraph text and button label when clicked. Also add an empty styles.css file for future styling.

--- scritps.js
+++ scritps.js
@@ -1,1 +1,7 @@
-alert("teste")
+const texto = document.getElementById("texto")
+const botao = document.getElementById("botao")
+ 
+botao.addEventListener("click",()=>{
+     texto.textContent="Você já está ganhando conhecimento."
+     botao.textContent="Resgatado"
+});
